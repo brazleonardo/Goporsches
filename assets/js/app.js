64a1c144a641c1784bd851682129ebb7cd076283
Braz Leonardo <brazleonardoaraujo@gmail.com>
2018-09-13 12:13:14 -0300

    Ajuste hover carrinho selecionar cartão

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,7 +68,7 @@
 	   * @method helperSVGLoading
 	   */
 		helperSVGLoading: function () {
-			var $svg = `
+			var $svg = `z
 				<div class="loader-svg">
 				  <svg version="1.1" id="loader-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 				      viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
